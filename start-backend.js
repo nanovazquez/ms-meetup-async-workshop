078893990eb2e25a6c00d6f47c2452d2e5c5255f
nanovazquez <marianodvazquez@gmail.com>
2017-02-13 21:01:41 -0800

    Update start-backend

--- start-backend.js
+++ start-backend.js
@@ -2,9 +2,9 @@ var exec = require('child_process').exec;
 
 var scripts = [
   'cd ui-facade && npm start --silent',
-  'cd apis/end/azure-manager && npm start --silent',
-  'cd apis/end/runtime-manager && npm start --silent',
-  'cd apis/end/worker-clouds && npm start --silent',
+  'cd apis/begin/azure-manager && npm start --silent',
+  'cd apis/begin/runtime-manager && npm start --silent',
+  'cd apis/begin/worker-clouds && npm start --silent',
 ];
 
 process.stdout.write('\033c');
